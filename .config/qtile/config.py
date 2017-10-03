5f496b4a7db21ca6e888ef9a079f5d04578c0027
felytic <felytic@gmail.com>
2017-10-03 17:19:36 +0300

    Init commit

---
+++ .config/qtile/config.py
@@ -0,0 +1,108 @@
+from libqtile.config import Key, Screen, Group, Drag, Click
+from libqtile.command import lazy
+from libqtile import layout, bar, widget
+
+mod = 'mod4'
+
+keys = [
+    # Switch between windows in current stack pane
+    Key([mod], 'k', lazy.layout.down()),
+    Key([mod], 'j', lazy.layout.up()),
+
+    # Move windows up or down in current stack
+    Key([mod, 'control'], 'k', lazy.layout.shuffle_down()),
+    Key([mod, 'control'], 'j', lazy.layout.shuffle_up()),
+
+    # Switch window focus to other pane(s) of stack
+    Key([mod], 'Tab', lazy.layout.next()),
+
+    # Swap panes of split stack
+    Key([mod, 'shift'], 'space', lazy.layout.rotate()),
+
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key([mod, 'shift'], 'Return', lazy.layout.toggle_split()),
+
+    Key([mod], 'Return', lazy.spawn('urxvt')),
+    Key([mod], 'g', lazy.spawn('google-chrome-stable')),
+
+    # Toggle between different layouts as defined below
+    Key([mod], 'space', lazy.next_layout()),
+    Key([mod], 'w', lazy.window.kill()),
+
+    Key([mod, 'control'], 'r', lazy.restart()),
+    Key([mod, 'control'], 'q', lazy.shutdown()),
+    Key([mod], 'r', lazy.spawncmd()),
+
+    Key([], 'XF86AudioRaiseVolume',
+        lazy.spawn('amixer -c 1 -q set Master 2dB+')),
+    Key([], 'XF86AudioLowerVolume',
+        lazy.spawn('amixer -c 1 -q set Master 2dB-')),
+    Key([], 'XF86AudioMute',
+        lazy.spawn('amixer -c 1 -q set Master toggle')),
+]
+
+groups = [Group(i) for i in '123456789']
+
+for i in groups:
+    # mod1 + letter of group = switch to group
+    keys.append(
+        Key([mod], i.name, lazy.group[i.name].toscreen())
+    )
+
+    # mod1 + shift + letter of group = switch to & move focused window to group
+    keys.append(
+        Key([mod, 'shift'], i.name, lazy.window.togroup(i.name))
+    )
+
+layouts = [
+    layout.Max(),
+    layout.Stack(num_stacks=2)
+]
+
+widget_defaults = dict(
+    font='xos4 Terminess Powerline',
+    fontsize=12,
+)
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.GroupBox(borderwidth=2, rounded=False),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Systray(),
+                widget.Sep(),
+                widget.Volume(device='hw:1'),
+                widget.Sep(),
+                widget.Battery(update_delay=5),
+                widget.Sep(),
+                widget.Clock(format=' %Y-%m-%d %a %H:%M'),
+            ],
+            22,
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], 'Button1', lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], 'Button3', lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], 'Button2', lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []
+main = None
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating()
+auto_fullscreen = True
+focus_on_window_activation = 'smart'
+extentions = []
---
+++ .vimrc
@@ -0,0 +1,117 @@
+filetype off
+
+set nocompatible
+
+set relativenumber
+set nu
+
+set tabstop=4
+set softtabstop=2
+set shiftwidth=2
+set noexpandtab
+
+set enc=utf-8
+set colorcolumn=80
+
+set incsearch
+set hlsearch
+
+set splitbelow
+set splitright
+set nowrap
+
+syntax on
+
+setlocal spell spelllang=en_us
+
+"split navigations
+nnoremap <C-J> <C-W><C-J>
+nnoremap <C-K> <C-W><C-K>
+nnoremap <C-L> <C-W><C-L>
+nnoremap <C-H> <C-W><C-H>
+
+
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'morhetz/gruvbox'
+Plugin 'scrooloose/nerdtree'
+Plugin 'Xuyuanp/nerdtree-git-plugin'
+Plugin 'airblade/vim-gitgutter'
+Plugin 'luochen1990/rainbow'
+Plugin 'Raimondi/delimitMate'
+Plugin 'tpope/vim-commentary'
+Plugin 'kien/ctrlp.vim'
+Plugin 'tpope/vim-surround'
+Plugin 'davidhalter/jedi-vim'
+Plugin 'majutsushi/tagbar'
+Plugin 'tmhedberg/SimpylFold'
+Plugin 'vim-scripts/indentpython.vim'
+Plugin 'scrooloose/syntastic'
+Plugin 'Valloric/YouCompleteMe'
+Plugin 'bling/vim-airline'
+Plugin 'tpope/vim-unimpaired'
+call vundle#end()
+filetype plugin indent on
+
+
+set background=dark
+let g:gruvbox_contrast_dark='hard'
+colorscheme gruvbox
+
+let g:rainbow_active = 1
+
+map <F4> :TagbarToggle<CR>
+let g:tagbar_autofocus = 0
+
+let g:SimpylFold_docstring_preview=1
+
+let python_highlight_all=1
+
+let g:ycm_autoclose_preview_window_after_completion=1
+map <leader>g  :YcmCompleter GoToDefinitionElseDeclaration<CR>
+
+let g:airline_powerline_fonts = 1
+
+let g:syntastic_always_populate_loc_list = 1
+
+"========================== NERD TREE =========================================
+
+map <f2> :NERDTreeToggle<CR>
+
+"close vim when only NERDTree is open
+autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif
+
+" files to hide in NERDTree
+let NERDTreeIgnore=['\~$', '\.pyc$', '\.pyo$', '\.class$', 'pip-log\.txt$', '\.o$']
+
+autocmd vimenter * NERDTree
+
+let g:NERDTreeIndicatorMapCustom = {
+    \ "Modified"  : "*",
+    \ "Staged"    : "+",
+    \ "Untracked" : "_",
+    \ "Renamed"   : "%",
+    \ "Unmerged"  : "═",
+    \ "Deleted"   : "x",
+    \ "Dirty"     : "D",
+    \ "Clean"     : "C",
+    \ 'Ignored'   : "I",
+    \ "Unknown"   : "?"}
+
+" File highlighting
+function! NERDTreeHighlightFile(ext, color)
+  exec 'autocmd filetype nerdtree highlight ' . a:ext.' ctermfg='. a:color
+  exec 'autocmd filetype nerdtree syn match ' . a:ext.' #^\s\+.*'.  a:ext.'$#'
+endfunction
+
+call NERDTreeHighlightFile('py', 'green')
+call NERDTreeHighlightFile('json', 'blue')
+call NERDTreeHighlightFile('js', 'yellow')
+call NERDTreeHighlightFile('log', 'gray')
+call NERDTreeHighlightFile('md', 'gray')
+call NERDTreeHighlightFile('conf', 'red')
+call NERDTreeHighlightFile('html', 'magenta')
+
+"==============================================================================
