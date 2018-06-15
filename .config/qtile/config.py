5e39780929168c5f0f9de840a0b3b6c088ea0711
felytic <felytic@gmail.com>
2018-06-15 09:41:12 +0300

    Upgrade Qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,5 +1,3 @@
-import os
-
 from libqtile.config import Key, Screen, Group, Drag, Click
 from libqtile.command import lazy
 from libqtile import layout, bar, widget
@@ -14,6 +12,7 @@ RETURN = 'Return'
 SPACE = 'space'
 ESC = 'Escape'
 
+
 keys = [
     # Switch between windows in current stack pane
     Key([WIN], 'k', lazy.layout.down()),
@@ -22,6 +21,7 @@ keys = [
     # Move windows up or down in current stack
     Key([WIN, CTRL], 'k', lazy.layout.shuffle_down()),
     Key([WIN, CTRL], 'j', lazy.layout.shuffle_up()),
+    Key([WIN], 'f', lazy.window.toggle_fullscreen()),
 
     # Switch window focus to other pane(s) of stack
     Key([WIN], TAB, lazy.layout.next()),
@@ -38,7 +38,7 @@ keys = [
 
     # Toggle between different layouts as defined below
     Key([WIN], SPACE, lazy.next_layout()),
-    Key([WIN], 'w', lazy.window.kill()),
+    Key([WIN], 't', lazy.window.kill()),
 
     Key([WIN, CTRL], 'r', lazy.restart()),
     Key([WIN, CTRL], 'q', lazy.shutdown()),
@@ -61,22 +61,35 @@ keys = [
     Key([WIN], 'x', lazy.spawn('xfe')),
 
     # Lock screen
-    Key([CTRL, ALT], 'l', lazy.spawn('slock')),
+    Key([], 'F10', lazy.spawn('slock')),
 
     # Previous group
-    Key([WIN], ESC, lazy.screen.toggle_group())
-]
+    Key([WIN], ESC, lazy.screen.toggle_group()),
 
+    # Print screen
+    Key([], 'Print', lazy.spawn('import screenshot.png')),
+]
 # ================================ GROUPS =====================================
 
-groups = [Group(i) for i in '1234567890']
+groups1 = [Group(i) for i in '1234567890']
+groups2 = [Group('F' + i) for i in '123456789']
+groups = groups1 + groups2
 
-for i in groups:
+for i in groups1:
     # win + letter of group = switch to group
     keys.append(
-        Key([WIN], i.name, lazy.group[i.name].toscreen())
+        Key([WIN], i.name, lazy.group[i.name].toscreen(0))
     )
 
+
+for i in groups2:
+    # win + letter of group = switch to group
+    keys.append(
+        Key([WIN], i.name, lazy.group[i.name].toscreen(1))
+    )
+
+
+for i in groups:
     # win + shift + letter of group = switch to & move focused window to group
     keys.append(
         Key([WIN, SHIFT], i.name, lazy.window.togroup(i.name))
@@ -102,8 +115,52 @@ screens = [
                     borderwidth=2,
                     rounded=False,
                     this_current_screen_border='7dbc2b',
-                    use_mouse_wheel=False
+                    use_mouse_wheel=False,
+                    disable_drag=True,
+                    visible_groups=[g.name for g in groups1]
                 ),
+                widget.Sep(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Systray(),
+                widget.Sep(),
+                widget.Volume(),
+                widget.Sep(),
+                widget.Memory(update_interval=1),
+                widget.MemoryGraph(
+                    line_width=1,
+                    margin_x=0,
+                    margin_y=1,
+                    border_width=0
+                ),
+                widget.Sep(),
+                widget.CPUGraph(
+                    line_width=1,
+                    margin_x=0,
+                    margin_y=1,
+                    border_width=0,
+                    type='line',
+                    graph_color='db3615',
+                    core=0
+                ),
+                widget.Sep(),
+                widget.Clock(format=' %Y-%m-%d %a %H:%M:%S'),
+            ],
+            22,  # bar height
+        ),
+    ),
+    Screen(
+        top=bar.Bar(
+            [
+                widget.GroupBox(
+                    borderwidth=2,
+                    rounded=False,
+                    this_current_screen_border='7dbc2b',
+                    use_mouse_wheel=False,
+                    disable_drag=True,
+                    visible_groups=[g.name for g in groups2]
+                ),
+                widget.Sep(),
                 widget.Prompt(),
                 widget.WindowName(),
                 widget.Systray(),
@@ -125,7 +182,6 @@ mouse = [
          start=lazy.window.get_position()),
     Drag([WIN], 'Button3', lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
-    Click([WIN], 'Button2', lazy.window.bring_to_front())
 ]
 
 dgroups_key_binder = None
@@ -136,5 +192,5 @@ bring_front_click = False
 cursor_warp = False
 floating_layout = layout.Floating()
 auto_fullscreen = True
-focus_on_window_activation = 'smart'
+focus_on_window_activation = 'focus'
 extentions = []
