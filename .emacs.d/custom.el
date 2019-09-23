f33418d1c3864ec4a4464a1d395d4018021b3c73
felytic <felytic@gmail.com>
2019-09-23 16:13:20 +0300

    Hello Emacs again

---
+++ .emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (treemacs-projectile treemacs-icons-dired treemacs-magit treemacs-evil treemacs color-theme-sanityinc-tomorrow key-chord vimrc-mode yaml-mode dockerfile-mode which-key spaceline spacemacs-theme dimmer git-gutter-fringe magit helm-ag helm-projectile helm projectile imenu-list highlight-indent-guides evil-surround evil-commentary neotree ein flycheck jedi linum-relative rainbow-delimiters gruvbox-theme evil use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
