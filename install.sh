ebaa6af9c629c000a28241c791525d83a40f8059
felytic <felytic@gmail.com>
2021-02-04 18:52:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,8 +10,14 @@ yay -S --noconfirm go cmake npm fzf git alsa-utils ctags p7zip grub \
     xorg-xbacklight bash-completion ttf-dejavu redshift gimp bluez-utils \
     jupyter-notebook firefox ntp telegram-desktop xf86-video-intel udiskie \
     xf86-input-synaptics rxvt-unicode-pixbuf zsh-syntax-highlighting-git \
-    powerline-fonts-git vim-plug-git nerd-fonts-fira-code
+    powerline-fonts-git vim-plug-git nerd-fonts-fira-code awesome slock ntpd \
+    maim xorg-xrandr
 
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+git clone https://github.com/deficient/battery-widget.git ~/.config/awesome/battery-widget
+
+# Enable time update on startup
+systemctl start ntpd.service
